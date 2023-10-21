05461ba22dddd8f09897f3e7ddae1425cf4dd73e
DeexithParand2k2 <deexith2002@gmail.com>
2023-10-21 21:07:31 +0530

    camera capture and state storage completed

--- ezpay-client/src/pages/Register.js
+++ ezpay-client/src/pages/Register.js
@@ -1,21 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Webcam from "react-webcam"
 
 /**
  * 
  * @returns {JSX.Element} - Returns JSX element
  */
+
+
+var framesLimit=5, timeGap=1;
+
 function Register() {
 
-  const [captured,setCaptured] = useState(false)
-  const [camOpen,setCamOpen] = useState(false)
-  const [frames,setFrames] = useState([])
+  const [captured,setCaptured] = useState(false) // check if all frames are captured
 
+  const [camOpen,setCamOpen] = useState(false) // open and close cam
+
+  const [frames,setFrames] = useState([]) // store frames in state
+  const [capturing,setCapturing] = useState(false) // start capture & end capture
+
+  const webcamRef = useRef(null)
+
+  //start capture
+  // function startCapture(){
+  //   setCapturing(true)
+
+  //   const interval = setInterval(()=>{
+
+  //     if(frames.length>framesLimit){
+  //       clearInterval(interval)
+  //       setCapturing(false)
+  //       setCaptured(true)
+  //     }
+
+  //     if(webcamRef.current){
+  //       var currFrame = webcamRef.current.getScreenshot()
+  //       setFrames((frames)=>([...frames,currFrame]))
+  //     }
+
+  //   },timeGap*1000)
+
+  // }
+
+  function startCapture() {
+
+    setCamOpen(true)
+
+    setCapturing(true);
+  
+    const interval = setInterval(() => {
+      if (webcamRef.current) {
+        var currFrame = webcamRef.current.getScreenshot();
+  
+        setFrames((frames) => {
+          const updatedFrames = [...frames, currFrame];
+          if (updatedFrames.length >= framesLimit) {
+            clearInterval(interval);
+            setCapturing(false);
+            setCaptured(true);
+            setCamOpen(false)
+          }
+          return updatedFrames;
+        });
+      }
+    }, timeGap * 1000);
+  }
+
+  
   //on unmounting - remove session storage
   useEffect(()=>{
   
     return() => {
-      if(!captured){
+      if(captured){
         sessionStorage.clear()
       }
     }
@@ -28,7 +83,7 @@ function Register() {
       alignItems:'center',
       gap:'20px',
       flexDirection: 'column',
-      margin:'20px'
+      margin:'20px',
     }}>
         <div style={{display:'flex',flexDirection:'row'}}>
           <p>Register&nbsp;</p>
@@ -37,8 +92,13 @@ function Register() {
 
         <div>
           {
+            !capturing ? <p>Yet to capture</p> : <p>Capturing... Frame {frames.length}</p>
+          }
+          {
             camOpen ? (
-              <Webcam height={600} width={600} />
+              <div>
+                <Webcam ref={webcamRef} style={{border:'2px solid lightgreen'}} height={600} width={600} />
+              </div>
             ) : (
               <div style={{
                 backgroundColor:'grey',
@@ -64,7 +124,48 @@ function Register() {
               setCamOpen(true)
             }
           }}>OPEN/CLOSE CAM</button>
-          <button style={{padding:'10px'}} type="button">Capture and Register</button>
+          <button style={{
+            padding:'10px'
+          }} 
+            type="button"
+            onClick={startCapture}
+          >
+            Capture and Register
+          </button>
+        </div>
+
+        <div>
+          <h3 style={{textAlign:'center'}}>
+            Captured Frames : 
+            {
+              capturing && !captured && <p>Ongoing</p>
+            }
+            {
+              captured && <p>Completed</p>
+            }
+          </h3>
+          <div style={{
+            width:'1000px',
+            gap:'5px',
+            color:'white',
+            display:'flex',
+            flexDirection:'row',
+            flexWrap: 'wrap',
+          }}>
+            {
+              frames && frames.length>0 && 
+              frames.map((frame,index)=>(
+                <img
+                  key={index}
+                  src={frame}
+                  alt={`Frame ${index}`}
+                  height={120}
+                  width={160}
+                >
+                </img>
+              ))
+            }
+          </div>
         </div>
         
     </div>
